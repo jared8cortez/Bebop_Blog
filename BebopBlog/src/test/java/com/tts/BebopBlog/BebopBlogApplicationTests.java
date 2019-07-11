4023d73acea08fedd06738d4b2c555f96bedea3f
Jared Cortez <jared8cortez@gmail.com>
2019-07-11 14:02:46 -0500

    minor update

--- BebopBlog/src/test/java/com/tts/BebopBlog/BebopBlogApplicationTests.java
+++ BebopBlog/src/test/java/com/tts/BebopBlog/BebopBlogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tts.TechTalentBlog;
+package com.tts.BebopBlog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TechTalentBlogApplicationTests {
+public class BebopBlogApplicationTests {
 
 	@Test
 	public void contextLoads() {
